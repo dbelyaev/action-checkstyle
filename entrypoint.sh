44a4d925bdaea0dad49aa4fa08d2b054d05f2de8
Dmitriy Belyaev <dmib@equinor.com>
2021-06-01 22:00:56 +0200

    action should respect default value from action.yml (currently it will re-write it)
    ignore DL4006 from hadolint
    add double quotes for parameter to prevent globbing
    install checkstyle using specified version

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,9 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 printenv
 ls
 
+# fetch checkstyle of a requested version
+wget -O - -q "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${INPUT_CHECKSTYLE_VERSION}/checkstyle-${INPUT_CHECKSTYLE_VERSION}-all.jar" > /checkstyle.jar
+
 exec java -jar /checkstyle.jar -c "${INPUT_CHECKSTYLE_CONFIG}" "${INPUT_WORKDIR}" -f xml \
   | reviewdog -f=checkstyle \
       -name="checkstyle" \
