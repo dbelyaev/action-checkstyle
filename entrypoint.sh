ca57ba19ae1e49dd117c911f2f863154aeffafb9
Dmitriy Belyaev <dbelyaev@users.noreply.github.com>
2023-07-28 11:33:40 +0200

    add checkstyle cmd default parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ exec java -jar /checkstyle.jar -c "${INPUT_CHECKSTYLE_CONFIG}" "${INPUT_WORKDIR}
   | reviewdog -f=checkstyle \
       -name="checkstyle" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
-      -filter-mode="${INPUT_FILTER_MODE}" \
-      -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+      -filter-mode="${INPUT_FILTER_MODE:-added}" \
+      -fail-on-error="${INPUT_FAIL_ON_ERROR:-false}" \
       -level="${INPUT_LEVEL}" \
       ${INPUT_REVIEWDOG_FLAGS}
