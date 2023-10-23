ce22320a33eb2862105b4708292f881f4b7fe408
Dmitriy Belyaev <dbelyaev@users.noreply.github.com>
2023-10-23 07:42:37 +0200

    add default installation of Checkstyle as part of Dockerfile build add depup monitoring of default Checkstyle version (and automation to update) update docs fix typos

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
 FROM eclipse-temurin:17-jre-alpine
 
-ENV REVIEWDOG_VERSION=v0.15.0
+ENV REVIEWDOG_VERSION v0.15.0
+ENV CHECKSTYLE_VERSION 10.12.2
 
 SHELL ["/bin/ash", "-eo", "pipefail", "-c"]
 
 # hadolint ignore=DL4006
 RUN apk --no-cache add git
 
+# pre-install reviewdog and checkstyle
 # hadolint ignore=DL4006
-RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION}
+RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b /usr/local/bin/ ${REVIEWDOG_VERSION} && \
+    mkdir -p /opt/lib && \
+    wget -q -O /opt/lib/checkstyle.jar https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${CHECKSTYLE_VERSION}/checkstyle-${CHECKSTYLE_VERSION}-all.jar
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,25 +3,33 @@ command -v reviewdog >/dev/null 2>&1 || { echo >&2 "reviewdog: not found"; exit 
 
 set -e
 
+# output some information
+{ echo "Pre-installed"; java -jar /opt/lib/checkstyle.jar --version; } | sed ':a;N;s/\n/ /;ba'
+
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
   cd "${GITHUB_WORKSPACE}" || exit
   git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
 fi
 
+# user supplied custom properties file parameter, define it
 if [ -n "${INPUT_PROPERTIES_FILE}" ]; then
   OPTIONAL_PROPERTIES_FILE="-p ${INPUT_PROPERTIES_FILE}"
 fi
 
+# user wants to use custom checkstyle version, try to install it
+if [ -n "${INPUT_CHECKSTYLE_VERSION}" ]; then
+  url="https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${INPUT_CHECKSTYLE_VERSION}/checkstyle-${INPUT_CHECKSTYLE_VERSION}-all.jar"
+
+  echo "Custom Checkstyle version has been configured: 'v${INPUT_CHECKSTYLE_VERSION}', try to download from ${url}"
+  wget -q -O /opt/lib/checkstyle.jar $url
+fi
+
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-# fetch checkstyle of a requested version
-url="https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${INPUT_CHECKSTYLE_VERSION}/checkstyle-${INPUT_CHECKSTYLE_VERSION}-all.jar"
+# run check
+{ echo "Run check with"; java -jar /opt/lib/checkstyle.jar --version; } | sed ':a;N;s/\n/ /;ba'
 
-echo "Download Checkstyle 'v${INPUT_CHECKSTYLE_VERSION}' from ${url}"
-wget -O - -q "$url" > /checkstyle.jar
-
-echo "Run Checkstyle check"
-exec java -jar /checkstyle.jar "${INPUT_WORKDIR}" -c "${INPUT_CHECKSTYLE_CONFIG}" ${OPTIONAL_PROPERTIES_FILE} -f xml \
+exec java -jar /opt/lib/checkstyle.jar "${INPUT_WORKDIR}" -c "${INPUT_CHECKSTYLE_CONFIG}" ${OPTIONAL_PROPERTIES_FILE} -f xml \
   | reviewdog -f=checkstyle \
       -name="checkstyle" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
