b0b681ee0b1cc781b32aa456a1ee138e50812ea0
Dmitriy Belyaev <6915328+dbelyaev@users.noreply.github.com>
2026-01-06 14:00:28 +0100

    chore: reduce code style violations in test data to meet GitHub API limitation for warning annotations (max 10)

--- testdata/java/Application.java
+++ testdata/java/Application.java
@@ -28,7 +28,7 @@ public class Application {
       String[] beanNames = ctx.getBeanDefinitionNames();
       Arrays.sort(beanNames);
       for (String beanName : beanNames)
-      {
+        {
           System.out.println(beanName);
       }
     };
